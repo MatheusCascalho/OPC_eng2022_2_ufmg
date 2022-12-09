1d8fdc4ce6b8d59349bfcbaab5feffcd4a0f0eb8
Arthur <arthurenus@ufmg.br>
2022-12-09 18:04:36 -0300

    Client py para comunicação com o server Sample Applications

--- opc/testemedium.py
+++ opc/testemedium.py
@@ -10,7 +10,7 @@ opc = OpenOPC.client()
 
 #opc.connect(opc.servers()[0], 'localhost')
 opc.connect('Matrikon.OPC.Simulation.1', 'localhost')
-tags = ['Random.Int1', 'Random.Int2']
+tags = ['Bucket Brigade.Int1', 'Random.Int2']
 while True:
     alarmes = opc.read(tags, group='Teste', update=1)
     print(alarmes)
